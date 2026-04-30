a5f0dcc31d000e3ecc9ec8cb2acf471e2d6a78c4
Hamskid1 <hameedlateef343@gmail.com>
2026-04-30 19:46:31 +0100

    editing Navbar and adding some features

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -4,6 +4,7 @@ import {Outlet} from "react-router-dom";
 import {useSelector} from "react-redux";
 import Navbar from "components/Navbar";
 
+
 const Layout = () => {
   return <Box width="100%" height="100%">
     <Box>
