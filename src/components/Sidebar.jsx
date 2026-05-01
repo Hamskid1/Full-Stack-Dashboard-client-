5e597c6b0a5f2c886b915d8ae991608a04455cdd
Hamskid1 <hameedlateef343@gmail.com>
2026-05-01 11:30:53 +0100

    import some important elements in sideBar components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+    ChevronLeft,
+    SettingsOutlined,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMonthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined,
+  } from "@mui/icons-material";  
+
+const SideBar = () => {
+  return <div>SideBar</div>;
+};
+
+export default SideBar;
